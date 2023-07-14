b0565e82dd20a91c0e6888ea4a5f6f1add0216a2
Jamie Cropley <jamie.cropley@protonmail.com>
2023-07-14 01:20:58 +0100

    Update

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,7 @@ module.exports = function(eleventyConfig) {
 
     // Copy `bundle.css` to the output folder
     eleventyConfig.addPassthroughCopy("bundle.css");
+
+    // favicon
+    eleventyConfig.addPassthroughCopy("favicon.svg");
 }
